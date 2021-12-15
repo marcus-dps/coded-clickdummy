a19055b7d9c96b967c8aecdebb313ba91c137640
marcus-dps <96205009+marcus-dps@users.noreply.github.com>
2021-12-15 21:34:54 +0100

    Basic click dummy setup 2 separate pages with invisible buttons

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
+import { Routes, Route } from "react-router-dom";
+import Button from "./Button";
+import Screen from "./Screen";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Screen image="/page1.png">
+              <Button to="/page2" x="175" y="380" w="120" h="40" />
+            </Screen>
+          }
+        />
+        <Route
+          path="/page2"
+          element={
+            <Screen image="/page2.png">
+              <Button to="/" x="50" y="390" w="100" h="40" />
+            </Screen>
+          }
+        />
+      </Routes>
     </div>
   );
 }
---
+++ src/Button.js
@@ -0,0 +1,18 @@
+import { Link } from "react-router-dom";
+
+const debug = false;
+
+export default function Button({ x, y, w, h, to }) {
+  return (
+    <Link
+      className={`Button ${debug ? "debug" : ""}`}
+      to={to}
+      style={{
+        left: `${x}px`,
+        top: `${y}px`,
+        width: `${w}px`,
+        height: `${h}px`
+      }}
+    />
+  );
+}
